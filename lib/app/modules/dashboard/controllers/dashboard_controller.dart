875e38b5255038595680e3aa1423a7973fe9815d
Agung Wahyudi <olipiskandar@gmail.com>
2023-02-24 11:19:32 +0700

    add response to listview builder

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,8 +1,16 @@
 // ignore_for_file: unnecessary_overrides
-
+import 'dart:async';
+import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:project_ujikom/app/data/entertainment_response.dart';
+import 'package:project_ujikom/app/data/headline_response.dart';
+import 'package:project_ujikom/app/data/sports_response.dart';
+import 'package:project_ujikom/app/data/technology_response.dart';
+import 'package:project_ujikom/app/utils/api.dart';
 
 class DashboardController extends GetxController {
+  final _getConnect = GetConnect();
+
   @override
   void onInit() {
     super.onInit();
@@ -16,5 +24,25 @@ class DashboardController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  Future<HeadlineResponse> getHeadline() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentResponse> getEntertainment() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return EntertainmentResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<SportResponse> getSports() async {
+    final response = await _getConnect.get(BaseUrl.sports);
+    return SportResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TechnologyResponse> getTechnology() async {
+    final response = await _getConnect.get(BaseUrl.technology);
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
   }
 }
